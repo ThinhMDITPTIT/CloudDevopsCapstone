b15aee8276eaa34b28afc8e052ab6d652b8e1f67
thinhmd <thinhmditptit@gmail.com>
2022-06-12 10:14:11 +0700

    Capstone Project Structural Initialization

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    html = "<h3>Hello World, my name is ThinhMD (TonyTMD)</h3>"
+    return html.format(format)
+
+if __name__ == "__main__":
+    # specify port = 80
+    app.run(host='0.0.0.0', port=80, debug=True) 
